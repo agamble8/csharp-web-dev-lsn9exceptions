877509299e05667ebe614b62c9395099b74a84e0
Amy Gamble <amymgambl@gmail.com>
2020-11-05 18:40:57 -0600

    ch 11 exercise complete

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,12 +7,35 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if(y == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else 
+            {
+                return x / y;
+            }
+
         }
 
         static int CheckFileExtension(string fileName)
         {
             // Write your code here!
+            int fileScore = 0;
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException();
+                
+            }
+            else if (fileName.EndsWith(".cs"))
+            {
+                fileScore += 1;
+                return fileScore;
+            } 
+            else
+            {
+                return fileScore;
+            }
         }
 
 
@@ -26,6 +49,36 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+
+            Console.WriteLine("Enter first integer: ");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second integer: ");
+            double y = Double.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(Divide(x, y));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Cannot divide by 0");
+            }
+
+
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    Console.WriteLine(student.Key + " File Score: " + CheckFileExtension(student.Value));
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(student.Key + " File name cannot be empty");
+                }
+                
+            }
+            
+            
 
 
         }
